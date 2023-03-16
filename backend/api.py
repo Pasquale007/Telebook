3c9392349b81681cd8b615f3e74d43cb83929d92
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-03-16 22:12:36 +0100

    add routes for user and address_book
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,15 @@
 # FastAPI
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException, status
 
+from pydantic import BaseModel
 # MySQL Connector
 import pymysql.cursors
+
+# zusätzlich
+import json
+
 
 app = FastAPI()
 # configs for FastAPI
@@ -26,18 +32,149 @@ config = {
     'password': 'example',
     'database': 'APP'
 }
-
 # Connect to the database
-connection = pymysql.connect(host=config.get('host'),
-                             user=config.get('user'),
-                             password=config.get('password'),
-                             database=config.get('database'),
-                             cursorclass=pymysql.cursors.DictCursor)
+connection = pymysql.connect(
+    host=config.get('host'),
+    user=config.get('user'),
+    password=config.get('password'),
+    database=config.get('database'),
+    cursorclass=pymysql.cursors.DictCursor)
 
-@app.get("/")
-def read_root():
+
+#======================================== USER ========================================
+
+class UserLogin(BaseModel):
+    name: str
+    password: str
+
+
+class UserSignUp(BaseModel):
+    name: str
+    password: str
+    email: str
+
+
+@app.post("/login")
+def login(user: UserLogin):
     with connection.cursor() as cursor:
-        # Read a single record
-        #cursor.execute("SELECT user_name, user_email, user_password FROM USERS WHERE user_name = 'user'")
-        cursor.execute("SELECT * FROM USERS;")
+        string =  ''' 
+            SELECT *
+            FROM users
+            WHERE
+            name = %s AND password = MD5(%s);
+        '''
+        cursor.execute(string, (user.name, user.password))
+        return cursor.fetchone()
+        
+
+@app.post("/signup")
+def signup(user: UserSignUp):
+    with connection.cursor() as cursor:
+        string = '''
+            INSERT INTO users (name, email, password, role)
+            VALUES (%s, %s, MD5(%s), "user")
+        '''
+        num = cursor.execute(string, (user.name, user.email, user.password))
+        if num >= 1:
+            return {"message":"Benutzer '{user.name}'wurde erfolgreich erstellt!"}
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Bei der Erstellung des Benutzer ist ein Felher aufgetreten. Bitte versuche es erneut.",
+        )
+
+#======================================== Adressbücher ========================================
+
+class Addressbook(BaseModel):
+    user_id: int
+    name: str
+
+@app.post("/addressbook")
+def create_addressbooks(addressbook: Addressbook):
+    with connection.cursor() as cursor:
+        # Check if an address book with same name already exists in the db
+        cursor.execute("SELECT id FROM address_books WHERE name = %s", (addressbook.name,))
+        existing_addressbook = cursor.fetchone()
+        if existing_addressbook:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Es existiert schon ein Adressbuch mit diesem Namen. Bitte ändere diesen Namen.",
+            )
+
+        string =  ''' 
+            INSERT INTO address_books (user_id, name) 
+            VALUES (%s, %s);
+        '''
+        num = cursor.execute(string, (addressbook.user_id, addressbook.name))
+        if num >= 1:
+            return  {"message": f"Adressbuch '{addressbook.name}' wurde erfolgreich erstellt!"}
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Bei der Erstellung des Adressbuches ist ein Felher aufgetreten. Bitte versuche es erneut.",
+        )
+
+
+@app.get("/addressbook")
+def get_all_addressbooks():
+    with connection.cursor() as cursor:
+        cursor.execute(" SELECT * FROM address_books;")
         return cursor.fetchall()
+
+
+@app.get("/addressbook/{addressbook_id}")
+def get_one_addressbooks(addressbook_id: int):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM address_books WHERE id = %s;", addressbook_id)
+        result = cursor.fetchone()
+        if result:
+            return result
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Adressbuch nicht gefunden",
+        )
+
+
+@app.put("/addressbook/{addressbook_id}")
+def update_addressbooks(addressbook_id: int, updated_addressbook: Addressbook):
+    with connection.cursor() as cursor:
+        # Check if address book exists
+        cursor.execute("SELECT id FROM address_books WHERE id = %s", addressbook_id)
+        existing_addressbook = cursor.fetchone()
+        if not existing_addressbook:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Das Adressbuch existiert nicht.",
+            )
+
+        # Check if updated address book name already exists
+        cursor.execute("SELECT id FROM address_books WHERE name = %s", updated_addressbook.name)
+        existing_addressbook = cursor.fetchone()
+        if existing_addressbook:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Ein Adressbuch mit diesem Namen existiert bereits. Bitte wählen sie einen anderen Namen.",
+            )
+
+        # if everything is ok -> update the address book
+        string = '''
+            UPDATE address_books 
+            SET  name = %s 
+            WHERE id = %s;
+        '''
+        old_name = updated_addressbook.name
+        num = cursor.execute(string, (updated_addressbook.name, addressbook_id))
+        if num >= 1:
+            return {"message": f"Das Adressbuch wurde zu '{old_name}' umgenannt."}
+
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Das Adressbuch konnte nicht aktualisiert werden. Bitte versuchen sie es später erneut.",
+        )
+
+
+@app.delete("/addressbook/{addressbook_id}")
+def delete_addressbooks(addressbook_id: int):
+    with connection.cursor() as cursor:
+        # Check if address book exists
+        num = cursor.execute("DELETE FROM address_books WHERE id = %s", addressbook_id)
+        if num >= 1:
+            return {"message": "Das Adressbuch wurde gelöscht"}
--- server/init.sql
+++ server/init.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS address_books (
   user_id INT UNSIGNED NOT NULL,
   name VARCHAR(255) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 /*insert values into address_books*/
@@ -44,15 +44,15 @@ CREATE TABLE IF NOT EXISTS contacts (
   city VARCHAR(255),
   zip_code VARCHAR(20),
   PRIMARY KEY (id),
-  FOREIGN KEY (address_book_id) REFERENCES address_books(id)
+  FOREIGN KEY (address_book_id) REFERENCES address_books(id) ON DELETE CASCADE
 );
 
 /*insert values into contact*/
 INSERT INTO contacts (address_book_id, first_name, last_name, email, address, city, zip_code)
 VALUES
-    (1, 'Jane', 'Doe', 'jane.doe@example.com', '123 Main St', 'Anytown', '12345'),
-    (2, 'Pascal', 'Thurow', 'pascal.thurow@example.com', 'Otto-Hahn Str 7', 'Oberkotzau', '95145'),
-    (1, 'Bob', 'Smith', 'bob.smith@example.com', '456 Elm St', 'Anytown', '12345');
+    (1, 'Julia', 'Wesel', 'julia.wesel@example.com', 'Baumstrasse 3a', 'Berlin', '00012'),
+    (1, 'Bob', 'Schmidt', 'bob.schmidt@example.com', 'Blumenweg 12', 'Hamburg', '54678'),
+    (2, 'Pascal', 'Thurow', 'pascal.thurow@example.com', 'Otto-Hahn Str 7', 'Oberkotzau', '95145');
 
 /*create table phone_numbers*/
 CREATE TABLE IF NOT EXISTS phone_numbers (
@@ -60,13 +60,13 @@ CREATE TABLE IF NOT EXISTS phone_numbers (
   contact_id INT UNSIGNED NOT NULL,
   phone_number VARCHAR(20) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (contact_id) REFERENCES contacts(id)
+  FOREIGN KEY (contact_id) REFERENCES contacts(id) ON DELETE CASCADE
 );
 
 
 /*insert values into phone_numbers*/
 INSERT INTO phone_numbers (contact_id, phone_number)
 VALUES 
-    (1, '555-1234'),
-    (2, '555-5678'),
-    (2, '555-9999');
+    (1, '12345678990'),
+    (2, '3847502194785'),
+    (2, '6584687');
