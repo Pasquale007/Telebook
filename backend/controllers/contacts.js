d09ce0a0f8b2d52ad57fad6b6dce7619f89047e4
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-05-31 12:21:04 +0200

    rfmeove debugging
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- backend/controllers/contacts.js
+++ backend/controllers/contacts.js
@@ -2,7 +2,6 @@ import { validationResult } from 'express-validator';
 import { createConnection } from "../db.js";
 
 export const createContact = (req, res) => {
-    console.log("adsf")
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
