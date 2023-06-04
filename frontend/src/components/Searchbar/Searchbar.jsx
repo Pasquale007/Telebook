ec3b94df1fe91f836efd689eb09148f2308a7a30
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-06-04 16:57:51 +0200

    fix for undefined
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- frontend/src/components/Searchbar/Searchbar.jsx
+++ frontend/src/components/Searchbar/Searchbar.jsx
@@ -1,9 +1,11 @@
 import { SearchOutlined } from "@ant-design/icons";
 import { Input } from "antd";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 
 export default function SearchBar({ setContacts, allContacts }) {
+    const [searchValue, setSearchValue] = useState("");
+
 
     const filterContacts = useCallback((inputString) => {
         if (inputString.length === 0) {
@@ -13,16 +15,19 @@ export default function SearchBar({ setContacts, allContacts }) {
         const contacts = allContacts.filter((contact) =>
             contact.first_name.toLowerCase().includes(inputString) ||
             contact.last_name?.toLowerCase().includes(inputString) ||
-            contact.first_name.toLowerCase().concat(' ' + contact.last_name?.toLowerCase()).includes(inputString) ||
+            contact.first_name.toLowerCase().concat(' ' + (contact.last_name || '').toLowerCase()).includes(inputString) ||
             contact.phone_numbers?.find(number => number.includes(inputString))
         )
         setContacts(contacts);
     }, [allContacts, setContacts]);
 
+    useEffect(() => {
+        filterContacts(searchValue);
+    }, [filterContacts, searchValue]);
 
     return (
         <Input
-            onChange={(e) => filterContacts(e.target.value)}
+            onChange={(e) => setSearchValue(e.target.value)}
             style={{ padding: "10px", width: "50%" }}
             prefix={<SearchOutlined />
             }
